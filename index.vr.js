b3b991440c7caf8fbe8417e6c38423bada39e1a0
Ryan Stelly <ryan.stelly@live.com>
2017-05-20 20:12:17 -0500

    a little cleanup for clarification

--- index.vr.js
+++ index.vr.js
@@ -7,28 +7,29 @@ import {
   View,
 } from 'react-vr';
 
-export default class redux_vr extends React.Component {
+export default class App extends React.Component {
   render() {
+    let textStyle = {
+      backgroundColor: '#777879',
+      fontSize: 0.8,
+      fontWeight: '400',
+      layoutOrigin: [0.5, 0.5],
+      paddingLeft: 0.2,
+      paddingRight: 0.2,
+      textAlign: 'center',
+      textAlignVertical: 'center',
+      transform: [{translate: [0, 0, -3]}],
+    };
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
         <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
+          style={textStyle}>
+          Hello there!
         </Text>
       </View>
     );
   }
 };
 
-AppRegistry.registerComponent('redux_vr', () => redux_vr);
+AppRegistry.registerComponent('redux_vr', () => App);
